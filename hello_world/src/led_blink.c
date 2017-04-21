c21f71c42586a12eb45341cc5f57bd161b542192
BonfaceKilz <bonfacemunyoki@gmail.com>
2017-04-21 20:16:01 +0300

    making some few changes to led_blink and reorganising the src

--- hello_world/src/led_blink.c
+++ hello_world/src/led_blink.c
@@ -1,23 +1,25 @@
-#define F_CPU 1000000UL
+/*
+ * A simple program that demos led blinking
+ */
+
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define F_CPU 1000000UL
 #define LED PD6
-
-// Some useful macros
 #define output_low(port, pin) port &= ~(1<<pin)
 #define output_high(port, pin) port |= (1<<pin)
 #define set_input(portdir, pin) portdir &= ~(1<<pin)
 #define set_output(portdir, pin) portdir |= (1<<pin)
 
 int main(void){
-  set_output(DDRD, LED);
+        set_output(DDRD, LED);
 
-  while(1){
-    output_high(PORTD, LED);
-    _delay_ms(200);
+        while(1){
+                output_high(PORTD, LED);
+                _delay_ms(200);
 
-    output_low(PORTD, LED);
-    _delay_ms(200);
-  }
+                output_low(PORTD, LED);
+                _delay_ms(200);
+        }
 }
